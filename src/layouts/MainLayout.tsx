da09f2619de7aa52cb0a9e9d811104fd91e6d506
pedrogilsenarego <pedrogilsenarego@gmail.com>
2023-07-27 20:29:03 +0100

    a

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,11 +2,28 @@ import { Box } from "@mui/material";
 
 const HomepageLayout = (props: any) => {
   return (
-    <>
-      <Box padding="20px 20px" height="100vh">
+    <Box
+      height="100vh"
+      width="100vw"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      style={{ backgroundColor: "whitesmoke" }}
+    >
+      <Box
+        padding="20px 20px"
+        height="100vh"
+        maxHeight="480px"
+        maxWidth="800px"
+        borderRadius="2px"
+        style={{
+          backgroundColor: "white",
+          boxShadow: "1px 1px 15px #00000066",
+        }}
+      >
         {props.children}
       </Box>
-    </>
+    </Box>
   );
 };
 
